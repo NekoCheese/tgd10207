1ad2b7db0ab6202647265faca1331191811f7488
NekoCheese <happaite4664@gmail.com>
2022-07-22 22:45:29 +0800

    輪播

---
+++ js_file/index.js
@@ -0,0 +1,20 @@
+// // 輪播
+// $(document).ready(function () {
+//   $(".owl-carousel").owlCarousel();
+// });
+
+// var owl = $(".owl-carousel");
+
+// owl.owlCarousel({
+//   items: 1,
+//   loop: true,
+//   autoplay: true,
+//   autoplayTimeout: 5000,
+//   autoplayHoverPause: true,
+//   animateOut: "fadeOut",
+// });
+
+// $(".owl-carousel").owlCarousel({
+//   loop: true,
+//   nav: true,
+// });

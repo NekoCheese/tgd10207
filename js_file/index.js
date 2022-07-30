e07eb2825c4e8e16ce01bad7b4e76dc332495423
NekoCheese <happaite4664@gmail.com>
2022-07-30 12:12:56 +0800

    合併forPC

---
+++ js_file/index.js
@@ -0,0 +1,7 @@
+let rwd_list = document.getElementsByClassName("bi-list")[0];
+
+rwd_list.addEventListener("click", function () {
+  let rwd_list_on = document.getElementsByClassName("top-nav")[0];
+  // rwd_list_on.nextElementSibling("top-nav").classList.add("-on");
+  console.log(this);
+});

2feed3c6e9a9492bc7cd44c61505d09bef5e2089
NekoCheese <happaite4664@gmail.com>
2022-08-01 19:29:17 +0800

    合併_微調RWD

--- js_file/index.js
+++ js_file/index.js
@@ -2,3 +2,76 @@
 $("#RWD_list").on("click mouseleave", function () {
   $(this).toggleClass("-on");
 });
+
+// 聯絡表單選項檢查
+$("#email").keyup(function () {
+  if ($(this).val() == "") {
+    $(".mail .error").html("請輸入電子郵件");
+  } else $(".mail .error").html("");
+});
+
+// // 姓名不為空
+$("#names").keyup(function () {
+  if ($(this).val() == "") {
+    $(".names .error").html("請輸入姓名");
+  } else $(".names .error").html("");
+});
+
+// 內容不為空
+$("#textarea").keyup(function () {
+  if ($(this).val() == "") {
+    $(".form_med .error").html("請輸入內容");
+  } else $(".form_med .error").html("");
+});
+
+// // 問題類型不為空
+$("#select_pgm").change(function () {
+  if ($(this).val() == null) {
+    $(".type .error").html("請選擇問題類型");
+  } else $(".type .error").html("");
+});
+
+// 聯絡表單送出檢查
+$("form").submit(function (e) {
+  let error_msg = "";
+
+  // 停止預設行為
+  e.preventDefault();
+
+  function emailAddress(email) {
+    var reg = new RegExp(
+      /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i
+    );
+    return reg.test(email);
+  }
+
+  // 信箱 反轉false送出錯誤警告
+  if (!emailAddress($("#email").val())) {
+    error_msg = error_msg + " 信箱資料 ";
+  }
+
+  // 姓名不為空
+  if ($("#names").val() == "") {
+    error_msg = error_msg + " 姓名資料 ";
+  }
+
+  if ($("#textarea").val() == "") {
+    error_msg = error_msg + " 未填寫問題內容 ";
+  }
+
+  // 問題類型不為空
+  if ($("#select_pgm").val() == null) {
+    error_msg = error_msg + " 問題類型 ";
+  }
+
+  if (!$("#agree").is(":checked")) {
+    error_msg = error_msg + " 尚未同意 ";
+  }
+
+  // 無誤後送出
+  if (error_msg == "") {
+    alert("資料成功送出");
+  } else {
+    $("#error").html(error_msg + "， 請再次確認");
+  }
+});

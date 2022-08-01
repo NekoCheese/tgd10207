78a7f1228a30e60a379af9b3807bf892451c2e37
NekoCheese <happaite4664@gmail.com>
2022-08-01 12:41:16 +0800

    合併

--- js_file/index.js
+++ js_file/index.js
@@ -1,7 +1,4 @@
-let rwd_list = document.getElementsByClassName("bi-list")[0];
-
-rwd_list.addEventListener("click", function () {
-  let rwd_list_on = document.getElementsByClassName("top-nav")[0];
-  // rwd_list_on.nextElementSibling("top-nav").classList.add("-on");
-  console.log(this);
+// RWD導覽列
+$("#RWD_list").on("click mouseleave", function () {
+  $(this).toggleClass("-on");
 });

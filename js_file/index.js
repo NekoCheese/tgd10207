f302307976295bb0bf548f09e5702b0d196e5d7f
NekoCheese <happaite4664@gmail.com>
2022-08-08 13:25:39 +0800

    合併

--- js_file/index.js
+++ js_file/index.js
@@ -125,14 +125,15 @@ $(".show_file").click(function (e) {
 // LIGHTBOX
 $(".lightbox").click(function () {
   let imgsrc = $(this).attr("src");
+  // let className = $(this).val();
   $("body").append(
     `<div class='img-popup'><div class="l_bk"><img src="` +
       imgsrc +
-      `"></div><div class='r_bk'><input type="button" value="Like" /><input type="button" value="Download" /><input type="button" value="Add to collection" /><ul><li>點級數 : ` +
+      `"></div><div class='r_bk'><input type="button" value="喜歡" /><input type="button" value="下載" /><input type="button" value="加入收藏" /><ul><li>瀏覽次數 : ` +
       Math.floor((Math.random() + 1) * 200) +
-      `</li><li>下載數 : ` +
+      `</li><li>下載次數 : ` +
       Math.floor(Math.random() * 50) +
-      `</li><li>分類</li></ul>
+      `</li><li>分類 : </li></ul>
 </div><span class='close-lightbox'>&times;</span></div>`
   );
   $(".close-lightbox, .img-popup").click(function () {

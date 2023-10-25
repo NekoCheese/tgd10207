4752aab94ed370e5dd49030272963989ef23b4ff
NekoCheese <happaite4664@gmail.com>
2023-10-25 21:25:04 +0800

    LQIP

--- js_file/index.js
+++ js_file/index.js
@@ -1,3 +1,29 @@
+// LQIP
+const lazyImages = document.querySelectorAll(".lazy");
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      const src = img.dataset.src;
+
+      // 載入完成後切換效果
+      img.onload = () => {
+        img.classList.add("lazy-out");
+        img.classList.remove("lazy");
+      };
+
+      img.setAttribute("src", src);
+
+      observer.unobserve(img);
+    }
+  });
+});
+
+lazyImages.forEach((image) => {
+  observer.observe(image);
+});
+
 // EMAIL_REGEXP
 function emailAddress(email) {
   var reg = new RegExp(
